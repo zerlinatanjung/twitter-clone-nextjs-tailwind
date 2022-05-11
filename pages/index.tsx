9c32baafbd4efd7fb5f7320a9a5d76622b809c15
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-11 18:31:18 +0700

    refactor: add GetServerSideProps, Tweet, fetchTweets in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -23,3 +29,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets()
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
