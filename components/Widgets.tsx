45a86bd748dbb4beb74f7afefeb7fe216b9e0790
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-11 16:27:42 +0700

    style: add style in Widgets component

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div className="mt-2 px-2">
+    <div className="mt-2 px-2 col-span-2 hidden lg:inline">
       {/* Search */}
       <div className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 p-3">
         <SearchIcon className="h-5 w-5 text-gray-400" />
