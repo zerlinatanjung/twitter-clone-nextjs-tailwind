e546f83f63ba4b562691b22b6c4e07e66ddc9d4c
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-21 13:53:29 +0700

    refactor: add style in Tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -51,7 +51,7 @@ function Tweet({ tweet }: Props ) {
         <div className='flex justify-between mt-5'>
             <div className="flex cursor-pointer items-centers space-x-3 text-gray-400">
                 <ChatAlt2Icon className='h-5 w-5' />
-                <p>5</p>
+                <p>{comments.length}</p>
             </div>
 
             <div className="flex cursor-pointer items-centers space-x-3 text-gray-400">
@@ -70,10 +70,22 @@ function Tweet({ tweet }: Props ) {
         {/* Comment Box Logic */}
 
         {comments?.length > 0 && (
-            <div>
+            <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
                 {comments.map(comment => (
-                    <div key={comment._id}>
-                        <img src={comment.profileImg} alt="" />
+                    <div key={comment._id} className='relative flex space-x-2'>
+                        <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30'/>
+                        <img src={comment.profileImg} className='mt-2 h-7 w-7 object-cover rounded-full' alt="" />
+                        <div>
+                            <div className='flex items-center space-x-1'>
+                                <p className='mr-1 font-bold '>{comment.username}</p>
+                                <p className='hidden text-sm text-gray-500 lg:inline'>@{comment.username.replace(/\s+/g, '').toLowerCase()} •</p>
+                                <TimeAgo 
+                            className='text-gray-500'
+                            date={comment._createdAt}
+                            />
+                            </div>
+                            <p>{comment.comment}</p>
+                        </div>
                     </div>
                 ))}
             </div>
