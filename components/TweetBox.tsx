cc1a6153898d8903f8b1a7f2ccefebba5ef9fab4
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-21 13:54:32 +0700

    refactor: implementation useSession in TweetBox component

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -6,13 +6,15 @@ import {
     PhotographIcon,
     SearchCircleIcon,
 } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react'
 
 function Tweetbox() {
-    const [input, setInput] = useState<string>('') 
+    const [input, setInput] = useState<string>('')
+    const { data: session } = useSession()
 
   return (
     <div className='flex space-x-2 p-5'>
-        <img className='h-14 w-14 object-cover rounded-full mt-4' src="https://links.papareact.com/gll" alt=""/>
+        <img className='h-14 w-14 object-cover rounded-full mt-4' src={session?.user?.image || "https://links.papareact.com/gll"} alt=""/>
         <div className='flex flex-1 items-center pl-2'>
             <form className='flex flex-1 flex-col'>
                 <input 
@@ -27,7 +29,7 @@ function Tweetbox() {
                         <CalendarIcon className='h-5 w-5'/>
                         <LocationMarkerIcon className='h-5 w-5'/>
                     </div>
-                    <button disabled={!input} className='bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40'>
+                    <button disabled={!input || !session    } className='bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40'>
                         Tweet
                     </button>
                 </div>
