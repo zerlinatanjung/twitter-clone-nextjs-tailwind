8d64b9a0fe29906af5e441422e5d16347caab5a0
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-21 13:51:50 +0700

    refactor: add signIn, signOut, useSession to Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,10 @@ import {
   HomeIcon,
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session } = useSession()
   return (
     <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
       <img className="h-10 w-10 m-3" src="https://links.papareact.com/drq" alt="" />
@@ -21,7 +23,7 @@ function Sidebar() {
       <SidebarRow Icon={MailIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
       <SidebarRow Icon={CollectionIcon} title="Lists" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? 'Sign Out': 'Sign In'} />
       <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
   )
