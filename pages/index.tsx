bde2116c5c013f9c9c3789d730d384c12564f293
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-21 13:55:42 +0700

    refactor: add Toaster in index page and remove overflow-hidden style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
@@ -12,10 +13,11 @@ interface Props {
 
 const Home = ({ tweets }: Props) => {
   return (
-    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
+    <div className="lg:max-w-6xl mx-auto max-h-screen">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
+      <Toaster />
 
       <main className="grid grid-cols-9">
         <Sidebar />
