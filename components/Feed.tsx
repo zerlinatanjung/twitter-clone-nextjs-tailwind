f401195300a621923ec67536a1bab2309266adbe
Zerlina Tanjung <zerlinatanjung98@gmail.com>
2022-05-22 13:30:13 +0700

    refactor: add style and add setTweets

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -25,14 +25,14 @@ function Feed({tweets: tweetsProp}: Props) {
   } 
 
   return (
-    <div className="col-span-7 lg:col-span-5 border-x">
+    <div className="col-span-7 max-h-screen scrollbar-hide overflow-scroll lg:col-span-5 border-x">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon onClick={handleRefresh} className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
       </div>
 
       <div>
-        <TweetBox />
+        <TweetBox setTweets={setTweets} />
       </div>
 
       {/* Feed */}
